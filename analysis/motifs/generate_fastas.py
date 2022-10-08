c9848010cff9a85dcf9cdfd442316a2731637e01
Ahmed Hasan <ahmed.hasan@mail.utoronto.ca>
2022-10-08 13:38:02 -0400

    start sequence motif analysis

---
+++ analysis/motifs/generate_fastas.py
@@ -0,0 +1,160 @@
+"""
+generate_fastas.py - generate FASTA files containing recombinant sequence
+"""
+
+import csv
+import argparse
+import numpy as np
+from tqdm import tqdm
+from Bio import SeqIO
+
+def args():
+    parser = argparse.ArgumentParser(
+        description='generate fasta files w/ recombinant sequence', 
+        usage='python generate_fastas.py [options]')
+
+    parser.add_argument('-f', '--fname', required=True,
+        type=str, help='Crossovers file')
+    parser.add_argument('-w', '--window_size', required=True,
+        type=int, help='Amount of sequence to retrieve on either side of cross midpoint')
+    parser.add_argument('-d', '--fasta_dir', required=True,
+        type=str, help='Dir containing sample-chrom-separated FASTAs')
+    parser.add_argument('-r', '--random', required=True,
+        action='store_true', help='Generate equivalent amount of random sequences')
+    parser.add_argument('-o', '--out', required=True,
+        type=str, help='File to write to (.fa/.fasta)')
+
+    args = parser.parse_args()
+
+    return args.fname, args.window_size, args.fasta_dir, args.random, args.out
+
+lengths = {
+    'chromosome_01': 8225636,
+    'chromosome_02': 8655884,
+    'chromosome_03': 9286894,
+    'chromosome_04': 4130073,
+    'chromosome_05': 3682160,
+    'chromosome_06': 8913359,
+    'chromosome_07': 6492107,
+    'chromosome_08': 4526983,
+    'chromosome_09': 6807148,
+    'chromosome_10': 6800247,
+    'chromosome_11': 4479522,
+    'chromosome_12': 9952739,
+    'chromosome_13': 5281438,
+    'chromosome_14': 4217303,
+    'chromosome_15': 5870643,
+    'chromosome_16': 8042475,
+    'chromosome_17': 6954842,
+    'cpDNA': 205535,
+    'mtDNA': 15789 # including organelles for mt leakage
+    }
+
+def get_seq(parent1, parent2, line, window_size, fasta_dir, random, lengths):
+    """docstring
+
+    Parameters
+    ----------
+    parent1, parent2 : str
+        strings containing parental strains
+    line : dict
+        line output from csv.DictReader
+    window_size : int
+        window size in bp of sequence to include on either side
+    fasta_dir : str
+        path to dir containing sample-chr fastas
+    random : bool
+        if True, return randomly drawn seq from same chr (for null expectation)
+    lengths : dict
+        dict of chrom lengths
+
+    Returns
+    -------
+    total_seq : str
+        combined seq of length window_size * 2
+    midpoint : int
+        midpoint coord of sequence
+    """
+    if not parent1.startswith('GB') and not parent1.startswith('CC'):
+        parent1 = 'CC' + parent1
+    if not parent2.startswith('GB') and not parent2.startswith('CC'):
+        parent2 = 'CC' + parent2
+    chrom = line['chromosome']
+    if not random:
+        midpoint = int(line['midpoint'].split(':')[1])
+    elif random:
+        # draw random value across chrom
+        midpoint_range = [window_size, lengths[chrom] - window_size] # can't be edge of chrom
+        midpoint = np.random.randint(midpoint_range[0], midpoint_range[1])
+        
+    haps = list(set([hap for hap, pos, base, qual in eval(line['detection']) if hap != 'N']))
+    if len(haps) > 2:
+        print(line)
+        raise Exception('wtf')
+
+    # get order of fastas from which to get sequence
+    fastas = []
+    for hap in haps:
+        if hap == '1':
+            fastas.append(fasta_dir + f'{parent1}.{chrom}.fa')
+        elif hap == '2':
+            fastas.append(fasta_dir + f'{parent2}.{chrom}.fa')
+
+    # get positions on either end of midpoint
+    coords = [int(midpoint - window_size), midpoint, int(midpoint + window_size)]
+    seq_left = str(SeqIO.read(fastas[0], 'fasta')[coords[0]:coords[1]].seq)
+    seq_right = str(SeqIO.read(fastas[1], 'fasta')[coords[1]:coords[2]].seq)
+    total_seq = seq_left + seq_right
+    return total_seq, midpoint
+            
+
+def generate_fastas(fname, window_size, fasta_dir, random, lengths, out):
+    """docstring
+
+    Parameters
+    ----------
+    fname : str
+        path to CO list
+    window_size : int
+        window size in bp of sequence to include on either side
+    fasta_dir : str
+        path to dir containing sample-chr fastas
+    out : str
+        file to write to
+
+
+    Returns
+    -------
+    None
+    """
+    if not fasta_dir.endswith('/'):
+        fasta_dir += '/'
+    with open(out, 'w') as f_out:
+        with open(fname, 'r') as f_in:
+            reader = csv.DictReader(f_in, delimiter='\t')
+
+            for line in tqdm(reader):
+                cross = line['cross']
+                chrom = line['chromosome']
+                if chrom in ['cpDNA', 'mtDNA']:
+                    continue
+                if cross in ['3071x2931', '3071x3062']:
+                    continue
+                parent1, parent2 = cross.split('x')
+                seq, midpoint = get_seq(
+                    parent1, parent2, line, window_size, fasta_dir, random, lengths)
+                if not random:
+                    f_out.write(f'>{cross}-{chrom}:{midpoint}\n')
+                elif random:
+                    f_out.write(f'>{cross}-{chrom}:{midpoint}-random\n')
+                f_out.write(seq + '\n')
+
+def main():
+    fname, window_size, fasta_dir, random, out = args()
+    generate_fastas(fname, window_size, fasta_dir, random, lengths, out)
+
+if __name__ == '__main__':
+    main()
+
+        
+
---
+++ analysis/motifs/log.md
@@ -0,0 +1,134 @@
+
+## 7/10/2022
+
+today - motif searches in COs using meme 5.4.1, which has been installed
+in `~/apps` and can be found in `~/meme/bin`
+
+looks like MEME requires a fasta containing sequences of interest - let's do that
+for the three populations individually, and then all combined
+
+based on my brief MEME notebook from late 2017 - I can use `bedtools getfasta` to generate this
+fasta file:
+
+```bash
+# first need to get bed files from the CO files
+
+mkdir -p data/motifs/co_bed/
+
+for fname in data/phase_changes/crossovers_lasso/*; do
+    base=$(basename ${fname})
+    grep -v '3071x2931' ${fname} | grep -v '3071x3062' | cut -f 5,7,8 | tail -n +2 > data/motifs/co_bed/${base}
+done
+
+# sort - within co_bed dir
+for fname in *.tsv; do
+    base=$(basename ${fname} .tsv)
+    sort -k1,1 -k2,2n ${fname} > ${base}.sorted.tsv;
+done
+
+# bedtools getfasta - back out to project dir
+mkdir -p data/motifs/fasta
+
+for fname in data/motifs/co_bed/*sorted.tsv; do
+    base=$(basename ${fname} .sorted.tsv)
+    bedtools getfasta \
+        -fi data/references/CC4532.w_organelles_MTplus.fasta \
+        -bed ${fname} \
+        -fo data/motifs/fasta/${base}.fasta
+done
+```
+
+and now let's get MEMEing - 
+
+```bash
+mkdir -p data/motifs/meme_out
+
+# test on smallest file first
+time ./bin/meme data/motifs/fasta/crossovers_NA1_corrected.fasta \
+-o data/motifs/meme_out/ \
+-text -dna -V -p 8 \
+-nmotifs 10 \ # find up to 10 motifs
+-minsites 5 -maxsites 20
+```
+
+this is just a preliminary run - realistically I should be using the actual
+sequence of the parents/reads for this, with each the sequence of each rcmb event
+considered + extended about ~500 bp in either direction (Comeron 2012)
+
+the 'correct' way to do this would otherwise be to, for each CO:
+
+- get parents, midpoint, and read in 'detection' list with `eval`
+- get 250 bp on either side of midpoint from either parental fasta
+    - eg if detection is 1,2 and midpoint is 2000, get 1750-2000 from 1 and 2000-2250 from 2
+    - could use `SeqIO.index` for quick lazy loading of records this way
+    - I do think I already have chrom-separated fasta files from the ldhelmet analysis though which works
+- append to fasta file of CO sequences
+
+also need to similarly sample n (number of COs) randomly sampled 500 bp chunks of the genome
+and provide these to MEME with the `-neg` arg (file containing control sequences) 
+
+trying out a script that does this:
+
+```bash
+time python analysis/motifs/generate_fastas.py \
+--fname data/phase_changes/crossovers_lasso/crossovers_NA1_corrected.tsv \
+--window_size 250 \
+--fasta_dir data/ldhelmet/fasta \
+--out data/motifs/fasta/NA1_spliced.fasta # takes 1 min per 1k COs
+```
+
+looks good - and now to run MEME on this - this does require
+a custom alphabet since othe fastas have '\*' chars for some reason,
+so I've copied the DNA alphabet definition and added '\*' at the bottom
+as equivalent to N
+
+```bash
+time ./bin/meme data/motifs/fasta/NA1_spliced.fasta \
+-o data/motifs/meme_out/ \
+-text -V -p 8 -alph meme_alphabet.txt \
+-nmotifs 10 \ # find up to 10 motifs
+-minsites 5 -maxsites 20 > data/motifs/meme_out/NA1.meme
+```
+
+next up install ceqlogo and convert meme output - try generating the html
+output since maybe that'll automatically generate the seq logo figure
+
+## 8/10/2022
+
+let's try meme html output
+
+```bash
+time ./bin/meme data/motifs/fasta/NA1_spliced.fasta \
+-o data/motifs/meme_out_NA1/ \ # have to give new dir
+-V -p 8 -alph meme_alphabet.txt \
+-nmotifs 1  -minsites 5 -maxsites 20
+```
+
+this actually worked and generated the files + figures I needed! 
+
+let's rerun with 8 motifs - once I'm convinced this looks good,
+going to generate a set of randomly drawn control sequences as well
+
+```bash
+time ./bin/meme data/motifs/fasta/NA1_spliced.fasta \
+-oc data/motifs/meme_out_NA1/ \ # will overwrite prev dir
+-V -p 8 -alph meme_alphabet.txt \
+-nmotifs 8 -minsites 5 -maxsites 20 # took 18 min
+```
+
+looks good, and this seems to be returning a single significant motif - the final
+thing left to do is redo this with control sequences, and then run it on
+all three populations 
+
+updated `generate_fastas.py` so that it'll randomly draw midpoints
+if needed - 
+
+```bash
+time python analysis/motifs/generate_fastas.py \
+--fname data/phase_changes/crossovers_lasso/crossovers_NA1_corrected.tsv \
+--window_size 250 \
+--fasta_dir data/ldhelmet/fasta \
+--random \
+--out data/motifs/fasta/NA1_random.fasta # takes 1 min per 1k COs
+```
+
